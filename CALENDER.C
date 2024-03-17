abd8ff72b06f6c80b33266cc7690fdd303996681
Dhyana2004 <156940188+Dhyana2004@users.noreply.github.com>
2024-03-17 17:25:12 +0530

    Update CALENDER.C

--- CALENDER.C
+++ CALENDER.C
@@ -1,44 +1,46 @@
 #include<stdio.h>
 
- struct Week{
-   char day[100];
-   int date;
-   char actdisp[100];
- };
+struct Week {
+    char day[100];
+    int date;
+    char actdisp[100];
+};
 
- struct Week day[7];
+void read(struct Week day[], int numDays) {
+    for (int i = 0; i < numDays; i++) {
+        printf("Enter the date for day %d:\n", i + 1);
+        scanf("%d", &day[i].date);
 
+        printf("Enter the Day for day %d:\n", i + 1);
+        scanf("%s", day[i].day);
 
-   void read(){
-
-       for(int i = 7;i<=7;i++){
-       printf("Enter the date for week:\n");
-       scanf("%d",&day[i].date);
-
-       printf("Enter the Day for week:\n");
-       scanf("%s",day[i].day);
-
-       printf("Enter the activity discreption:\n");
-       scanf("%s",day[i].actdisp);
+        printf("Enter the activity description for day %d:\n", i + 1);
+        scanf("%s", day[i].actdisp);
     }
-   }
+}
 
-    void display(){
+void display(struct Week day[], int numDays) {
+    printf("------------Calendar---------\n");
 
-      printf("------------Calendar---------\n");
+    for (int i = 0; i < numDays; i++) {
+        printf("Date:%d, \t Day:%s, \t Activity Description:%s \n", day[i].date, day[i].day, day[i].actdisp);
+    }
+}
 
+int main() {
+    int numDays;
+    printf("How many days of activity list do you want? (up to 7): ");
+    scanf("%d", &numDays);
 
-      for(int i = 7; i<=7; i++){
-         printf("Date:%d, \t Day:%s, \t ActivityDEScription:%s \n",day[i].date,day[i].day,day[i].actdisp);
-      }
-
+    if (numDays > 7 || numDays < 1) {
+        printf("Invalid number of days. Please choose between 1 and 7.\n");
+        return 1;
     }
 
-     int main(){
+    struct Week day[7];
 
+    read(day, numDays);
+    display(day, numDays);
 
-         read();
-         display();
-
-         return 0;
-     }
+    return 0;
+}
