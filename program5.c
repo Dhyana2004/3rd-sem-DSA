d07e7a5ac7692259e3ace2104ac80f3b37328658
Dhyana2004 <156940188+Dhyana2004@users.noreply.github.com>
2024-03-18 11:43:21 +0530

    Update program5.c

--- program5.c
+++ program5.c
@@ -1,43 +1,79 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<math.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
 
- int i,top=-1,op1,op2,res,s[20];
- char postfix[20],symb;
+#define MAX_SIZE 20
 
-void push(int item){
-    s[++top]=item;
+int top = -1;
+int s[MAX_SIZE];
+
+void push(int item) {
+    if (top == MAX_SIZE - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    s[++top] = item;
 }
-int pop(){
-    return (s[top--]);
+
+int pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    return s[top--];
 }
 
- void main(){
-    printf("Enter postfix expression:");
-    scanf("%s",postfix);
+int evaluatePostfix(char postfix[]) {
+    int i, op1, op2;
+    char symb;
 
-    for(int i=0;postfix[i]!='\0';i++){
-        symb=postfix[i];
-        if(isdigit(symb)){
-            push(symb-'0');
-        }else{
-            op2=pop();
-            op1=pop();
-
-                 switch(symb){
-                    case '+':push(op1+op2);
-                             break;
-                    case '-':push(op1-op2);
-                             break;
-                    case '*':push(op1*op2);
-                             break;
-                    case '/':push(op1/op2);
-                             break;
-                    case '^':push(pow(op1,op2));
-                             break;
-                 }
+    for (i = 0; postfix[i] != '\0'; i++) {
+        symb = postfix[i];
+        if (isdigit(symb)) {
+            push(symb - '0');
+        } else {
+            op2 = pop();
+            op1 = pop();
+            switch (symb) {
+                case '+':
+                    push(op1 + op2);
+                    break;
+                case '-':
+                    push(op1 - op2);
+                    break;
+                case '*':
+                    push(op1 * op2);
+                    break;
+                case '/':
+                    if (op2 == 0) {
+                        printf("Division by zero\n");
+                        return 0;
+                    }
+                    push(op1 / op2);
+                    break;
+                case '^':
+                    // Implementing exponentiation with a loop
+                    int result = 1;
+                    for (int j = 0; j < op2; j++) {
+                        result *= op1;
+                    }
+                    push(result);
+                    break;
+                default:
+                    printf("Invalid operator\n");
+                    return 0;
+            }
         }
     }
-    res=pop();
-    printf("Result:%d",res);
- }
+    return pop();
+}
+
+int main() {
+    char postfix[MAX_SIZE];
+    printf("Enter postfix expression: ");
+    scanf("%s", postfix);
+    int result = evaluatePostfix(postfix);
+    if (result != -1)
+        printf("Result: %d\n", result);
+    return 0;
+}
